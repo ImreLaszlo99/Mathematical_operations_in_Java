b0859794cbd0d3f959fe40a93324fd4c70530eb1
Imre László <imrelaca25@gmail.com>
2025-09-08 11:49:14 +0200

    Add files via upload

---
+++ Min_and_Max_Whatif/src/If_min_max/What_If_Min_and_Max.java
@@ -0,0 +1,28 @@
+package If_min_max;
+
+public class What_If_Min_and_Max {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 8;
+		int c = 5;
+		
+		int min = 0; 
+		int max = 10;
+		
+		
+		if (a < c && b > a);
+			min = c;
+			max = b; 
+		
+		System.out.println("Min and max values:");
+		System.out.println("min =" + min);
+		System.out.println("max =" + max);
+		
+		
+
+	}
+	
+	
+}
